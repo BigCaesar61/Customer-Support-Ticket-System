5c2a6ca01fe42276e55b2b31426e189acd8da55b
Martin Leone <martinnikolaslll@gmail.com>
2024-11-03 16:03:11 -0500

    Dynamically display tickets on the page.

--- main.js
+++ main.js
@@ -3,21 +3,25 @@
 const supportTickets = document.getElementById('supportTickets');
 
 
-async function displaySupportTickets(supportTicketsTickets) {
+async function displaySupportTickets(supportTickets) {
     try {
         const supportTickets = await fetch(`https://jsonplaceholder.typicode.com/posts/${supportTickets}`);
         if (!supportTickets.ok) {
             throw new Error('Failed to fecth support tickets');
         }
+        //Task 3: Display Tickets Dynamically on the Page
+        const tickets = await supportTickets.json();
+        console.log('Tickets:', tickets);
 
-         
-        
+        tickets[0].user.forEach(user => {
+            const listItem = document.createElement('li');
+            listItem.textContent = `User ID: ${user.userId}, Title: ${user.title}`;
+            orderList.appendChild(listItem);
+        });
     } catch (error) {
         console.error('Error:', error.message);
     } finally  { //task 4: Use finally to Ensure Cleanup
         console.log('fetch completed');
-        
-
     }
 } 
 
