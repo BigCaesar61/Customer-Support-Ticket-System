3908c0117852dbd81cf52d49ab8680181de9cdd8
Martin Leone <martinnikolaslll@gmail.com>
2024-11-03 15:15:04 -0500

    Implement async/await to fetch ticket data and handle errors.

--- main.js
+++ main.js
@@ -2,3 +2,15 @@
 
 const supportTickets = document.getElementById('supportTickets');
 
+
+async function displaySupportTickets(userId) {
+    try {
+        const customerTicket = await fetch(`https://jsonplaceholder.typicode.com/posts/${userId}`);
+        if (!customerTicket.ok) {
+            throw new Error('Failed to fecth support tickets');
+        }
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+} 
+
